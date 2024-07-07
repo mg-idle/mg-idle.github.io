aa99784b7794979d93c0cd9e6e4f61e919ce3756
Mikael Siidorow <mikael@siidorow.com>
2024-07-07 12:28:13 +0300

    add service worker for offline support

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,6 +17,9 @@ const config = {
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : /** @type {`/${string}`} */ (process.env.BASE_PATH)
+		},
+		version: {
+			name: child_process.execSync('git rev-parse HEAD').toString().trim()
 		}
 	}
 };
